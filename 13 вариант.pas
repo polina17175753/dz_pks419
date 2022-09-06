eeae778e9fde395d0887563221325024527deaa7
polina17175753 <112984326+polina17175753@users.noreply.github.com>
2022-09-06 23:55:59 +0300

    Add files via upload

---
+++ 13 вариант.pas
@@ -0,0 +1,14 @@
+﻿type st = array[1..3] of string[20];
+const en: st = ('animal', 'word', 'body');
+      ru: st = ('животное', 'слово ', 'тело');
+var i: integer;
+    s, p: string;
+begin
+  write('Введите английское слово: ');
+  readln(s);
+  p := '';
+  for i := 1 to 3 do if s = en[i] THEN p := ru[i];
+  if p <> ''
+    then writeln('Перевод: ', p)
+    else writeln('Этого слова нет в словаре')
+end.
