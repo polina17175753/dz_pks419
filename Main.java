323df54394fa383146b0d067d6c6a061544c6581
Peso4ek <67576795+Peso4ekForever@users.noreply.github.com>
2022-09-06 16:44:16 +0300

    фикс говно-кода

--- Main.java
+++ Main.java
@@ -2,9 +2,10 @@ import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
+    private static final int MAX_WORD_LENGTH = 8;
+
     //Федоров Андрей НОМЕР 19
     public static void main(String[] args) {
-
         System.out.println("Введите строку");
         Scanner input = new Scanner(System.in);
         String currentWord = input.next();
@@ -27,39 +28,26 @@ public class Main {
         int di = input.nextInt();
 
         System.out.println("Ответ = " + task2(numbersInput, b, di));
-
     }
 
     //Задача 1
     public static boolean task1(String word, String subString) {
-
-        int wordLeight = 8;
-        if (word.length() > wordLeight) {
-            return false;
-        }
-
-        return word.contains(subString);
+        return word.length() < MAX_WORD_LENGTH && word.contains(subString);
     }
 
     //Задача 2
     public static float task2(int[] numbersArray, int b, int di) {
-
         int summaUp = 0;
         int summaDown = 0;
 
-        //Сумма Числителя
-        for (int i = 1; i < 11; i++) {
-            int number = numbersArray[i - 1];
+        //Сумма Числителя и Знаменателя
+        for (int i = 0; i < 10; i++) {
+            int number = numbersArray[i];
             summaUp += ((number * number) - b);
-        }
-
-        //Сумма Знаменателя
-        for (int i = 1; i < 11; i++) {
-            int number = numbersArray[i - 1];
             summaDown += (number - b);
         }
 
-        return (summaUp / summaDown) - (numbersArray[di - 1] - b);
+        return (float) (summaUp / summaDown) - (numbersArray[di - 1] - b);
     }
 }
 
