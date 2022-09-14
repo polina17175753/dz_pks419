82900a0d865e07215e9455ca566d627d6d644a39
polina17175753 <112984326+polina17175753@users.noreply.github.com>
2022-09-15 01:46:39 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,51 +1,12 @@
-import java.util.Scanner;
-
 public class Main {
-    private static final int MAX_WORD_LENGTH = 8;
+    public static void main(String[]args) {
+        Student student = new Student("Popova", "Polina","PKS-419",4);
+        Aspirant aspirant = new Aspirant("Smirnov","Pavel","PKS-419",4,"Kursach");
+        Student sweet = new Aspirant("Petrov","Dima","PKS-419",5,"Rabota");
 
-    //Федоров Андрей НОМЕР 19
-    public static void main(String[] args) {
-        System.out.println("Введите строку");
-        Scanner input = new Scanner(System.in);
-        String inputWord = input.next();
+        System.out.println("Имя: " + student.firstName + " " + student.lastName + ", группа: " + student.group + ", средняя оценка: " + student.averageMark + ", стипендия: " + student.getscolarship());
+        System.out.println("Имя: " + aspirant.firstName + " " + aspirant.lastName + ", группа: " + aspirant.group + ", средняя оценка: " + aspirant.averageMark + ", научная работа: " + aspirant.nauchnayaRabota + ", стипендия: " + aspirant.getscolarship());
+        System.out.println("Имя: " + sweet.firstName + " " + sweet.lastName + ", группа: " + sweet.group + ", средняя оценка: " + sweet.averageMark + ", стипендия: " + sweet.getscolarship());
 
-        System.out.println(checkValidWord(inputWord, "кн"));
-
-        int[] inputNumbers = new int[10];
-        for (int i = 0; i < 10; i++) {
-            System.out.printf("Введите %d элемент массива%n", i + 1);
-            inputNumbers[i] = input.nextInt();
-        }
-
-        System.out.println("Введите число b");
-        int b = input.nextInt();
-        System.out.println("Введите число di");
-        int di = input.nextInt();
-
-        System.out.printf("Ответ = %f%n", calculateSum(inputNumbers, b, di));
-    }
-
-    //Задача 1
-    public static String checkValidWord(String word, String subString) {
-        if (word.length() < MAX_WORD_LENGTH && word.contains(subString)) {
-            return word;
-        }
-        return "Вхождений не найдено, или слово превышает 8 символов!";
-    }
-
-    //Задача 2
-    public static float calculateSum(int[] numbersArray, int b, int di) {
-        int summaUp = 0;
-        int summaDown = 0;
-
-        //Сумма Числителя и Знаменателя
-        for (int i = 0; i < 10; i++) {
-            int number = numbersArray[i];
-            summaUp += ((number * number) - b);
-            summaDown += (number - b);
-        }
-
-        return (float) (summaUp / summaDown) - (numbersArray[di - 1] - b);
     }
 }
-
