de0b7415b4887c4a9163d7678eb00b6ff85c1090
polina17175753 <112984326+polina17175753@users.noreply.github.com>
2022-09-15 21:43:08 +0300

    Delete Guitar.java

--- Guitar.java
+++
@@ -1,8 +0,0 @@
-public class Guitar implements Tool {
-    int NumberOfStrings;
-    Guitar( int NumberOfStrings){ this.NumberOfStrings = NumberOfStrings;}
-    @Override
-    public void play() {
-        System.out.printf("Играет Гитара, количество струн: %s %s ", NumberOfStrings, "  ");
-    }
-}
